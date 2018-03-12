214332e3dbfd6ae16ee75fb865e594c0346ce83f
WUCHANGS <907689522@qq.com>
2018-03-12 22:37:00 +0800

    文件操作更新

---
+++ FileOperation/FileOperation/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  FileOperation
+//
+//  Created by wu on 2018/3/12.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+- (IBAction)button:(id)sender;
+
+@end
+
---
+++ FileOperation/FileOperation/ViewController.m
@@ -0,0 +1,62 @@
+//
+//  ViewController.m
+//  FileOperation
+//
+//  Created by wu on 2018/3/6.
+//  Copyright © 2018年 xtz_pioneer. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "WUFileOperation.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)button:(id)sender {
+    //获取documents路径
+    NSString * documentsDirPath=[WUFileOperation documentsDir];
+    //文件夹名称
+    NSString * folderName=@"test";
+    //拼接文件夹路径
+    NSString * folderPath=[NSString stringWithFormat:@"%@/%@",documentsDirPath,folderName];
+    //创建文件夹
+    if ([WUFileOperation createDirectoryAtPath:folderPath]) {
+        NSLog(@"文件夹创建成功");
+        NSLog(@"文件夹路径:%@",folderPath);
+    }else{
+        NSLog(@"文件夹创建失败");
+    }
+    //文件名称
+    NSString * fileName=@"text.txt";
+    //拼接文件路径
+    NSString * filePath=[NSString stringWithFormat:@"%@/%@",folderPath,fileName];
+    //创建文件
+    if ([WUFileOperation createFileAtPath:filePath]) {
+        NSLog(@"文件创建成功");
+        NSLog(@"文件路径:%@",filePath);
+    }else{
+        NSLog(@"文件创建失败");
+    }
+    //向文件中写入内容
+    if ([WUFileOperation writeFileAtPath:filePath content:self.textView.text]) {
+        NSLog(@"文件内容写入成功");
+        NSLog(@"写入内容:%@",self.textView.text);
+    }else{
+        NSLog(@"文件内容写入失败");
+    }
+}
+@end
+
